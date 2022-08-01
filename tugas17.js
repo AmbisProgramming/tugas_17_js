6b8c49137516ae95d5e1e94fdd77f3a03efa28b6
AmbisProgramming <110365940+AmbisProgramming@users.noreply.github.com>
2022-08-01 13:06:24 +0700

    Add files via upload

---
+++ tugas17.js
@@ -0,0 +1,13 @@
+let string1 = "Saya beLajar bahaSa peMrograman deNgan khUsuk." ;
+console.log(string1.toLowerCase()) ;
+console.log(string1.toUpperCase()) ;
+
+let string2 = "Allen SedAng MemAncinG IkaN di SuNgai, DIA MEndapat IkAN GaBuS 3 Ekor dan LanGsuNg Dijual.";
+console.log(string2.toLowerCase());
+console.log(string2.toUpperCase());
+
+
+let string3 = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
+console.log(string3.charCodeAt(0) + "-" + string3.charCodeAt(1) + "-" + string3.charCodeAt(2)
++ "-" + string3.charCodeAt(3) + "-" + string3.charCodeAt(4) +  "-" + string3.charCodeAt(5)
+);
